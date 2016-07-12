abcc53d9c168f94bd3ec76b4769fcf26f182c932
David Ross <david@djr.com>
2016-07-11 22:22:13 -0400

    add documentation to converter script

--- resources/convertPlaintextToJSDict.py
+++ resources/convertPlaintextToJSDict.py
@@ -1,5 +1,39 @@
+# Convert PTF to HKF
+# Convert a plaintext file like BungeePixel.txt to a javascript mapping in hugs and kisses format
+
+# Plaintext Font Format
+# a glyph begins with a new line and a pound sign
+# the character represented by the glyph follows the pound sign
+# all following lines up to the next pound sign belong to that glyph
+# asterisks represent black, spaces represent white, new lines are new lines
+
+# for example:
+"""
+#a
+  *****  
+ ******* 
+ *** *** 
+ *** *** 
+ ******* 
+ ******* 
+ *** *** 
+ *** *** 
+         
+"""
+
+# and here is the same glyph converted to hugs and kisses:
+"""
+"a": "XXOOOOOXX\nXOOOOOOOX\nXOOOXOOOX\nXOOOXOOOX\nXOOOOOOOX\nXOOOOOOOX\nXOOOXOOOX\nXOOOXOOOX\nXXXXXXXXX\n",
+"""
+
+
+
 import string
-with open(u"/Users/david/Desktop/bungeePixel/bungeePixel.txt") as pixelFontFile:
+import os
+
+basePath = os.path.split(os.path.split(__file__)[0])[0]
+print '{'
+with open(os.path.join(basePath, "BungeePixel.txt")) as pixelFontFile:
     pixelFontLines = pixelFontFile.readlines()
     glyphs = []
     thisGlyph = ''
@@ -16,4 +50,5 @@ with open(u"/Users/david/Desktop/bungeePixel/bungeePixel.txt") as pixelFontFile:
     glyphs.append((thisName, thisGlyph))
 
     for gname, glyph in glyphs:
-        print '"%s": "%s",' %(gname, glyph)
+        print '\t"%s": "%s",' %(gname, glyph)
+print '}'
